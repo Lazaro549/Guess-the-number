327981fedee9598bb52648b174b78a150e6766d3
Lazaro Gomez Vitolo <lazarogvitolo@gmail.com>
2026-03-02 04:59:52 -0300

    Create Player.php

---
+++ src/Player.php
@@ -0,0 +1,20 @@
+<?php
+// src/Player.php
+
+namespace GuessTheNumber;
+
+class Player
+{
+    public static function getInput(string $prompt = "Enter your guess: "): int
+    {
+        echo $prompt;
+        $input = trim(fgets(STDIN));
+
+        if (!is_numeric($input)) {
+            echo "Please enter a valid number.\n";
+            return self::getInput($prompt);
+        }
+
+        return (int)$input;
+    }
+}
